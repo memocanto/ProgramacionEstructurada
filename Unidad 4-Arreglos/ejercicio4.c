300fb9f13fdbc682b58ceca33639cae2fe92080c
memocanto <46583130+memocanto@users.noreply.github.com>
2019-03-14 16:50:13 -0600

    Update ejercicio4.c

--- Unidad 4-Arreglos/ejercicio4.c
+++ Unidad 4-Arreglos/ejercicio4.c
@@ -1,8 +1,8 @@
 /*
 Autor: Guillermo Canto
 Entradas: Ninguna
-Salidas: Ninguna
-Procedimiento general: Llena un vector de tama�o 100 con valores aleatorios del 1 al 100 (calificaciones)
+Salidas: Los 20 promedios de las calificaciones generadas.
+Procedimiento general: Llena un vector de tama?o 100 con valores aleatorios del 1 al 100 (calificaciones)
 Calcula los promedios de cada alumno si cada 5 calificaciones en el vector antes mencionado corresponde 
 a un alumno diferente y los almacena en un vector de promedios.
 */
@@ -11,40 +11,46 @@ a un alumno diferente y los almacena en un vector de promedios.
 #include <stdio.h>
 #include<stdlib.h>
 #include<time.h>
-#define N 100
-int numeroAleatorio(int valorMax);
-void asignarValores(int arreglo[N]);
-void calcularPromedios(int calificaciones[], int promedios[]);
+
+int numeroAleatorio();
+void asignarValores(int arreglo[100]);
+void calcularPromedios(int calificaciones[100], int promedios[20]);
+void imprimirResultados(int arreglo[20]);
 int main(int argc, char *argv[]) {
 	srand(time(NULL));
 	int calificaciones[100]; 
 	int promedios[20];
 	asignarValores(calificaciones);
 	calcularPromedios(calificaciones, promedios);
+	imprimirResultados(promedios);
 	return 0;
 }
 
 
-int numeroAleatorio(int valorMax) {
-	int n;
-	n = rand()%valorMax;
+int numeroAleatorio(){
+	int n = rand()%101;
 	return n;
 }
-void asignarValores(int arreglo[N]){
+void asignarValores(int arreglo[100]){
 	for (int i=0; i<100; i++){
-		arreglo[i]= numeroAleatorio(100);
+		arreglo[i]= numeroAleatorio();
 	}
 }
-void calcularPromedios(int calificaciones[N], int promedios[N]){
-	int numAlumno=0;
-	for(int i=0; i<20; i++){
-		for(int j=numAlumno*5; j< (numAlumno*5)+5 ; j++){
-			promedios[i]= promedios[i]+ calificaciones[j];
+void calcularPromedios(int calificaciones[100], int promedios[20]){
+	for(int i = 0; i < 20; i++){
+		for(int j = i*20; j < (i*20)+5; j++){
+				promedios[i] += calificaciones[j]; 
 		}
 		promedios[i]= promedios[i]/5;
-		numAlumno++;
 	}
 }
+
+void imprimirResultados(int arreglo[20]){
+	for(int i = 0; i < 20; i++){
+		printf("Promedio %i : %i\n",i+1,arreglo[i]);
+	}
+}
+
 
 /**
  * QA: Luis Gerardo Leon Ortega
